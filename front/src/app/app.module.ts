6eb6a11aabb5707aa4c794c48b816c252282c53f
VitielloL <lucasvitielloo@gmail.com>
2020-10-30 20:31:22 -0300

    components do template

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,16 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Template
+import { HeaderComponent } from './components/template/header/header/header.component';
+import { FooterComponent } from './components/template/header/footer/footer.component';
+import { NavComponent } from './components/template/header/nav/nav.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

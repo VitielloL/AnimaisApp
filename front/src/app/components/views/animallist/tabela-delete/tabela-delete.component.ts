c5c8b9c5c9120cf109ac7ba54d1569b32fbc315b
VitielloL <lucasvitielloo@gmail.com>
2020-10-31 02:12:57 -0300

    tabela delete - falta função delete

--- front/src/app/components/views/animallist/tabela-delete/tabela-delete.component.ts
+++ front/src/app/components/views/animallist/tabela-delete/tabela-delete.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AnimalService, animalUnico } from '../animal.service';
 
 @Component({
   selector: 'app-tabela-delete',
   templateUrl: './tabela-delete.component.html',
-  styleUrls: ['./tabela-delete.component.scss']
+  styleUrls: ['./tabela-delete.component.scss'],
 })
 export class TabelaDeleteComponent implements OnInit {
+  nome;
+  dadoAnimal: animalUnico;
+  grupoAnimal;
+  local;
+  id;
+  lerUm;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private animalService: AnimalService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+      this.animalService.read1Animal(this.id).subscribe((dadoAnimal) => {
+        this.dadoAnimal = dadoAnimal;
+        this.nome = dadoAnimal.animal.nome;
+        this.local = dadoAnimal.animal.local;
+        this.grupoAnimal = dadoAnimal.animal.especie;
+      });
+    });
+  }
+  // delete no service e no ts.
+  delete() {
+    // this.tabelaReadService.delete(this.dadoAnimal.animais[0]).subscribe(() => {
+    //   this.router.navigate(['/listaCliente']);
+    // });
   }
 
+  cancel(): void {
+    this.router.navigate(['/animallist']);
+  }
 }

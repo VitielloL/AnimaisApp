89ca27448ddd43f2a662bd801bdde0708e895c9d
VitielloL <lucasvitielloo@gmail.com>
2020-10-31 02:06:30 -0300

    tabela create

--- front/src/app/components/views/animallist/tabela-create/tabela-create.component.ts
+++ front/src/app/components/views/animallist/tabela-create/tabela-create.component.ts
@@ -1,36 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AnimalService } from '../animal.service';
 
 @Component({
   selector: 'app-tabela-create',
   templateUrl: './tabela-create.component.html',
-  styleUrls: ['./tabela-create.component.scss']
+  styleUrls: ['./tabela-create.component.scss'],
 })
 export class TabelaCreateComponent implements OnInit {
   nome;
   grupoAnimal;
   local;
   criar;
-  
-  constructor() { }
 
-  ngOnInit(): void {
-    criarAnimal() {
-      console.log(this.nome, this.grupoAnimal, this.local);
-      this.criar = this.tabelaReadService
-        .create({
-          nome: this.nome,
-          especie: this.grupoAnimal,
-          local: this.local,
-        })
-        .subscribe(() => {
-          this.router.navigate(['/listaCliente']);
-        });
-    }
-  
-    cancel(): void {
-      this.router.navigate(['/listaCliente']);
-    }
-  }
+  constructor(private animalService: AnimalService, private router: Router) {}
+
+  ngOnInit(): void {}
+  criarAnimal() {
+    console.log(this.nome, this.grupoAnimal, this.local);
+    this.criar = this.animalService
+      .create({
+        nome: this.nome,
+        especie: this.grupoAnimal,
+        local: this.local,
+      })
+      .subscribe(() => {
+        this.router.navigate(['/animallist']);
+      });
   }
 
+  cancel(): void {
+    this.router.navigate(['/animallist']);
+  }
 }
